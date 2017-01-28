35c60e914d2e0ca35e26042f46001b5c6586e676
nasa9084 <nasa9084@live.jp>
2017-01-29 00:22:20 +0900

    add: comment to examples

--- mersenne_twister_example.go
+++ mersenne_twister_example.go
@@ -2,6 +2,8 @@ package mt
 
 import "fmt"
 
+// ExampleMersenneTwisterInt32 is an example of
+// 32bit unsigned int random value generation
 func ExampleMersenneTwisterInt32() {
 	key := []uint32{0x123, 0x234, 0x345, 0x456}
 	InitByArray(key)
@@ -10,6 +12,8 @@ func ExampleMersenneTwisterInt32() {
 	// 1067595299
 }
 
+// ExampleMersenneTwisterFloat64 is an example of
+// 32bit real random value generation
 func ExampleMersenneTwisterFloat64() {
 	key := []uint32{0x123, 0x234, 0x345, 0x456}
 	InitByArray(key)
